1c8e67cd3906a6afe1e075814c480ee61989c6b1
Adel <htwr86@gmail.com>
2019-12-05 20:03:48 +0300

    removed materializecss and react-materialize and add tailwind

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-//<!-- Compiled and minified CSS -->
-//<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-import "materialize-css/dist/css/materialize.min.css"
-// <!-- Compiled and minified JavaScript -->
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-import "materialize-css/dist/js/materialize.min.js"
--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-import { Navbar } from "react-materialize"
 
 const header = () => {
   return (
     <>
-      <nav>
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">
-            Logo
-          </Link>
-          <Link to="/" data-target="mobile-demo" className="sidenav-trigger">
-            <i className="material-icons">menu</i>
-          </Link>
-          <ul className="right hide-on-med-and-down">
-            <li>
-              <Link to="/">Sass</Link>
-            </li>
-            <li>
-              <Link to="/">Components</Link>
-            </li>
-            <li>
-              <Link to="/">Javascript</Link>
-            </li>
-            <li>
-              <Link to="/">Mobile</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <ul className="/" id="mobile-demo">
-        <li>
-          <Link to="/">Sass</Link>
-        </li>
-        <li>
-          <Link to="/">Components</Link>
-        </li>
-        <li>
-          <Link to="/">Javascript</Link>
-        </li>
-        <li>
-          <Link to="/">Mobile</Link>
-        </li>
-      </ul>
+      <h1>Header</h1>
     </>
   )
 }
