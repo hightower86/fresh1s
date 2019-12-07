a789ed0f36d73ef4b06ad92f2f4aa9d2af4a7553
Adel <htwr86@gmail.com>
2019-12-07 21:15:20 +0300

    add logo and burger to navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,23 @@ import { Link } from "gatsby"
 
 const header = () => {
   return (
-    <nav className="antialiased bg-gray-200">
-      <div className="flex"></div>
+    <nav className="lg:px-16 px-3 bg-white flex flex-wrap items-center lg:py-0 py-2">
+      <div className="flex-1 flex justify-between items-center">
+        <Link to="/">LOGO</Link>
+        <label for="menu-toggle" className="point-cursor lg:hidden block">
+          <svg
+            className="fill-current text-gray-900"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+          >
+            <title>menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+          </svg>
+        </label>
+        <input type="checkbox" className="hidden" id="menu-toggle" />
+      </div>
     </nav>
   )
 }
