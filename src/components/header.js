f27f90602f9c0e514cc33b7200860bb0ac561b70
Adel <htwr86@gmail.com>
2019-12-07 23:54:11 +0300

    working on burger

--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,19 @@ import { Link } from "gatsby"
 
 const header = () => {
   return (
-    <nav className="lg:px-16 px-3 bg-white flex flex-wrap items-center lg:py-0 py-2">
+    <header className="lg:px-16 px-3 bg-white flex flex-wrap items-center lg:py-0 py-2">
       <div className="flex-1 flex justify-between items-center">
         <Link to="/">LOGO</Link>
-        <label for="menu-toggle" className="point-cursor lg:hidden block">
-          <svg
-            className="fill-current text-gray-900"
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-          >
-            <title>menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-          </svg>
+        <label for="menu-toggle" className="cursor-pointer lg:hidden block">
+          <div className="w-50 h-20 bg-red-300">
+            <div className="w-4 h-px2 bg-gray-900"></div>
+            <div className="w-4 h-px2 bg-gray-900 my-3"></div>
+            <div className="w-4 h-px2 bg-gray-900"></div>
+          </div>
         </label>
         <input type="checkbox" className="hidden" id="menu-toggle" />
       </div>
-    </nav>
+    </header>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    height: {
+      px2: "2px",
+      px3: "3px",
+      px4: "4px",
+    },
+    margin: {
+      0.1: "0.1rem",
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
