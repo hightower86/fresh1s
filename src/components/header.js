1803f5a8cd1a032bf5efa655c512affd94ce11b7
Adel <htwr86@gmail.com>
2019-12-10 23:01:10 +0300

    solving problem with added css classes

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,22 @@ import logo from "../img/logo.png"
 
 const header = () => {
   return (
-    <header className="">
-      <div className="class">
-        <img src={logo} alt="logo" />
+    <header className="flex items-center justify-between p-4 bg-gray-900">
+      <div className="">
+        <img className=" h-8" src={logo} alt="logo" />
       </div>
       <div className="class">
-        <button type="button"></button>
+        <button
+          type="button"
+          class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+        >
+          <svg class="h-8 w-8 fill-current" viewBox="0 0 24 24">
+            <path
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
       </div>
     </header>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-module.exports = {
-  theme: {
-    extend: {},
-    height: {
-      px2: "2px",
-      px3: "3px",
-      px4: "4px",
-    },
-    margin: {
-      01: "0.1rem",
-      02: "0.2rem",
-    },
-  },
-  variants: {},
-  plugins: [],
-}
+// module.exports = {
+//   theme: {
+//     extend: {},
+//     height: {
+//       px2: "2px",
+//       px3: "3px",
+//       px4: "4px",
+//     },
+//     margin: {
+//       01: "0.1rem",
+//       02: "0.2rem",
+//     },
+//   },
+//   variants: {},
+//   plugins: [],
+// }
