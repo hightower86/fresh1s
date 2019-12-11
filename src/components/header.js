6da88cac49ce6e00407745883edf2c2b69cf2996
Adel <htwr86@gmail.com>
2019-12-11 20:06:08 +0300

    main menu

--- src/components/header.js
+++ src/components/header.js
@@ -9,12 +9,12 @@ import logo from "../img/logo.png"
 const Header = () => {
   const [menuActive, setMenuActive] = useState(true)
   return (
-    <header className="bg-gray-900">
-      <div className="flex items-center justify-between p-3 px-4">
+    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+      <div className="flex items-center justify-between py-3 px-4 sm:p-0">
         <div className="">
-          <img className=" h-8" src={logo} alt="logo" />
+          <img className=" h-6" src={logo} alt="logo" />
         </div>
-        <div>
+        <div className="sm:hidden">
           <button
             type="button"
             class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
@@ -42,7 +42,8 @@ const Header = () => {
       </div>
       <div
         className={
-          (menuActive ? "block " : "hidden ") + "px-2 pt-2 pb-4 text-white"
+          (menuActive ? "block " : "hidden ") +
+          "sm:flex  px-2 pt-2 pb-4 text-white sm:p-0"
         }
       >
         <Link
@@ -53,13 +54,13 @@ const Header = () => {
         </Link>
         <Link
           to="/"
-          className="mt-1 px-2 py-1 block font-semibold rounded hover:bg-gray-800"
+          className="mt-1 px-2 py-1 block font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
         >
           About
         </Link>
         <Link
           to="/"
-          className="mt-1 px-2 py-1 block font-semibold rounded hover:bg-gray-800"
+          className="mt-1 px-2 py-1 block font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
         >
           Contacts
         </Link>
